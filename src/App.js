4353656289378c751a5be51147e37db308407352
moskowitza <moskowitzaaron@mac.com>
2018-06-21 13:15:55 -0400

    adding a new src to begin work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React from "react";
-import Calculator from "./components/Calculator";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import FriendCard from "./components/FriendCard";
+import friends from "./friends.json";
 
-const App = () => <Calculator />;
+const App = () => (
+  <Wrapper>
+    <Title>Friends List</Title>
+    <FriendCard
+      name={friends[0].name}
+      image={friends[0].image}
+      occupation={friends[0].occupation}
+      location={friends[0].location}
+    />
+    <FriendCard
+      name={friends[1].name}
+      image={friends[1].image}
+      occupation={friends[1].occupation}
+      location={friends[1].location}
+    />
+    <FriendCard
+      name={friends[2].name}
+      image={friends[2].image}
+      occupation={friends[2].occupation}
+      location={friends[2].location}
+    />
+  </Wrapper>
+);
 
 export default App;
